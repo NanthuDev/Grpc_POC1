e08207e920b4e8c2739939b96307849d19f56f74
Nantha <nanthu1593@gmail.com>
2024-07-18 23:48:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const grpc = require("@grpc/grpc-js");
-
 const protoLoader = require("@grpc/proto-loader");
-
 const packageDefination = protoLoader.loadSync("todo.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDefination);
-
 const todoPackage = grpcObject.todoPackage;
 
 var server = new grpc.Server();
 
-server.addService(todoPackage.Todo.service, {
+ server.addService(todoPackage.Todo.service, {
   createTodo: createTodo,
   readTodos: readTodos,
 });
@@ -18,15 +15,22 @@ server.bindAsync(
   "0.0.0.0:3000",
   grpc.ServerCredentials.createInsecure(),
   (err, port) => {
-    if(err){
-        console.log("Err in Proto Init")
+    if (err) {
+      console.log("Err in Proto Init");
     }
-    console.log("Started-",'--',port)
+    console.log("Started-", "--", port);
   }
 );
- 
 
-function createTodo(call, callback) {
-  console.log(call);
+const todos = [];
+
+function createTodo(call, callback) { 
+  console.log(call.request);
+  const item = {
+    id: todos.length + 1,
+    text: call.request.text,
+  };
+  todos.push(item);
+  callback(null, item);
 }
 function readTodos(call, callback) {}
